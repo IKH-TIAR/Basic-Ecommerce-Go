e5fc9ed7ac490541a6fb380e2a5a9e179ef7e4ee
Ikhtiar <ikhti407@gmail.com>
2026-03-09 00:47:08 +0600

    updated cors middleware

--- middleware/corsMiddleware.go
+++ middleware/corsMiddleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // (3)
+		w.Header().Set("Access-Control-Allow-Origin", "*") 
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Content-Type", "application/json")
 
