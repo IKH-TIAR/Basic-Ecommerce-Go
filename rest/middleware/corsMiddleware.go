69eb3f17c660d77e8bdf58b56bacc02373cd067e
Ikhtiar <ikhti407@gmail.com>
2026-03-28 06:31:11 +0600

    added authorization cors

--- rest/middleware/corsMiddleware.go
+++ rest/middleware/corsMiddleware.go
@@ -1,15 +1,16 @@
 package middleware
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 )
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("CorsMiddleare")
-		w.Header().Set("Access-Control-Allow-Origin", "*") 
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Content-Type", "application/json")
 
 		next.ServeHTTP(w, r)
